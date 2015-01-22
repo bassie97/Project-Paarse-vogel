09a4e9248aada991b061ca523aeda8c48305e9e9
joshua van den hof <joshuavdhof@live.nl>
2015-01-22 13:32:25 +0100

    diersoort bear toegevoegt en simulater aangepast dat bear word weergegeven

--- foxes-and-rabbits-v2/Simulator.java
+++ foxes-and-rabbits-v2/Simulator.java
@@ -21,9 +21,11 @@ public class Simulator
     // The default depth of the grid.
     private static final int DEFAULT_DEPTH = 80;
     // The probability that a fox will be created in any given grid position.
-    private static final double FOX_CREATION_PROBABILITY = 0.02;
+    private static final double FOX_CREATION_PROBABILITY = 0.05;
     // The probability that a rabbit will be created in any given grid position.
     private static final double RABBIT_CREATION_PROBABILITY = 0.08;    
+    // The probability that a rabbit will be created in any given grid position.
+    private static final double BEAR_CREATION_PROBABILITY = 0.005;
 
     // List of animals in the field.
     private List<Animal> animals;
@@ -63,6 +65,7 @@ public class Simulator
         view = new SimulatorView(depth, width);
         view.setColor(Rabbit.class, Color.orange);
         view.setColor(Fox.class, Color.blue);
+        view.setColor(Bear.class, Color.red);
 
         view.eenStap.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) 
@@ -164,6 +167,11 @@ public class Simulator
                     Location location = new Location(row, col);
                     Rabbit rabbit = new Rabbit(true, field, location);
                     animals.add(rabbit);
+                }
+                else if(rand.nextDouble() <= BEAR_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Bear bear = new Bear(true, field, location);
+                    animals.add(bear);
                 }
                 // else leave the location empty.
             }
